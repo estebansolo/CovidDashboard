f689c65301bc48b4839ee42c87b274475f966dd3
Esteban Solorzano <estebansolorzano27@gmail.com>
2020-05-31 15:17:53 -0500

    Basic integration Covid 19 Dataset | 2 Graphs

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
+import plotly.express as px
+
+import pandas as pd
 
 app = dash.Dash(__name__)
 
@@ -17,6 +21,15 @@ colors = {
     'text': '#7FDBFF'
 }
 
+covid = pd.read_csv("covid_19.csv")
+covid["Date"] = pd.to_datetime(covid["Date"])
+covid_by_date = covid.groupby([covid["Date"]]).sum()
+
+dates = covid_by_date.index
+confirmed = covid_by_date["Confirmed"].values
+deaths = covid_by_date["Deaths"].values
+recovered = covid_by_date["Recovered"].values
+
 app.layout = app.layout = html.Div([
     html.H1("Dashboard using Dash", style={
         'textAlign': 'center',
@@ -27,8 +40,9 @@ app.layout = app.layout = html.Div([
     dcc.Graph(
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                {'x': dates, 'y': confirmed, 'type': 'linear', 'name': 'Confirmed'},
+                {'x': dates, 'y': deaths, 'type': 'linear', 'name': 'Deaths'},
+                {'x': dates, 'y': recovered, 'type': 'linear', 'name': 'Recovered'},
             ],
             'layout': {
                 'plot_bgcolor': colors['background'],
@@ -36,9 +50,22 @@ app.layout = app.layout = html.Div([
                 'font': {
                     'color': colors['text']
                 },
-                'title': 'Basic Data Visualization',
-                "xaxis": {'title': 'X Axis'},
-                "yaxis": {'title': 'Y Axis'},
+                'title': 'Covid 19',
+                "xaxis": {'title': 'Date'},
+                "yaxis": {'title': 'Cases'},
+            }
+        }
+    ),
+    dcc.Graph(
+        figure={
+            "data": [
+                go.Pie(
+                    labels=['Actives', 'Deaths', 'Recovered'],
+                    values=[(confirmed[-1] - deaths[-1] - recovered[-1]), deaths[-1], recovered[-1]]
+                )
+            ],
+            "layout": {
+                "title": 'Cases Percentage'
             }
         }
     )
