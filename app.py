198667f7d51c3a421793ce113b159f8b7be5f854
Esteban Solorzano <estebansolorzano27@gmail.com>
2020-05-31 12:49:04 -0500

    Basic use of Dash library

---
+++ app.py
@@ -0,0 +1,10 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash(__name__)
+
+app.layout = html.H1(children="Dashboard using Dash")
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
