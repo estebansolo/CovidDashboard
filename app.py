6c153f3713d0c3089849ae72f84d61e72a161f99
Esteban Solorzano <estebansolorzano27@gmail.com>
2020-05-31 12:52:18 -0500

    Using Markdown

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ import dash_html_components as html
 
 app = dash.Dash(__name__)
 
-app.layout = html.H1(children="Dashboard using Dash")
+markdown_text = """
+# Dashboard using Dash
+
+- Flask
+- Plotly.js
+- React.js
+
+[Dash Gallery](https://dash-gallery.plotly.host/Portal/)
+"""
+
+app.layout = dcc.Markdown(markdown_text)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
