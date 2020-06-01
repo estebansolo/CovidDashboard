e3e8c760981cf5c1b74ef8d03f5d8768f74ae77b
Esteban Solorzano <estebansolorzano27@gmail.com>
2020-05-31 19:06:21 -0500

    Using callback on interval to change the live update

--- updates.py
+++ updates.py
@@ -13,7 +13,17 @@ colors = {
 
 app.layout = app.layout = html.Div([
     html.Label("Interval"),
-    dcc.Graph(id="random_graph", animate=True),
+    dcc.RadioItems(
+        options=[
+            {'label': '1 second', 'value': 1},
+            {'label': '5 seconds', 'value': 5},
+            {'label': '10 seconds', 'value': 10}
+        ],
+        value=1,
+        id="interval_options",
+        labelStyle={'display': 'inline-block'}
+    ),
+    dcc.Graph(id="random_graph"),
     dcc.Interval(id='interval_component', interval=1000, n_intervals=0)
 ])
 
@@ -22,7 +32,6 @@ app.layout = app.layout = html.Div([
     [Input(component_id='interval_component', component_property='n_intervals')]
 )
 def update_graph(input_value):
-    print(input_value)
     countries = {
         "Colombia": random.randint(0, 100),
         "España": random.randint(0, 100),
@@ -44,6 +53,13 @@ def update_graph(input_value):
             'title': 'Covid 19',
         }
     }
+
+@app.callback(
+    Output(component_id="interval_component", component_property="interval"),
+    [Input(component_id="interval_options", component_property="value")]
+)
+def update_interval(interval_value):
+    return interval_value * 1000
 
 if __name__ == "__main__":
     app.run_server(debug=True)
